8a766dd74c534db8a19723646aab5569f6b4bb54
Rodrigo Rocha <rodrigomtc82@gmail.com>
2021-06-17 09:19:05 -0300

    create user route

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+const usersRoute = require("./api/routes/users");
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+mongoose.connect(
+  "mongodb://localhost/dogs",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("😎Connected to mongodb😎");
+  }
+);
+
+app.use("/users", usersRoute);
 
 app.listen(3333, () => {
   console.log(`⚡Server is running on port 3333⚡`);
